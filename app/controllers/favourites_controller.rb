37cd0a0e17b16a47545d8f8d369227f32162f657
Sam singh <samar2k97@gmail.com>
2024-06-13 12:16:30 +1000

    favourites-page

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,13 +1,25 @@
 class FavouritesController < ApplicationController
   def index
+    @favourite = Favourite.all
   end
 
   def new
+    @favourite = Favourite.new(favourite_params)
   end
 
   def create
+    @favourite = Favourite.new(favourite_params)
+    @favourite.save
   end
 
   def destroy
+    @favourite = Favourite.find(params[:id])
+    @favourite.destroy
+  end
+
+  private
+
+  def favourite_params
+    params.require(:favourite).permit(:user_id, :recipe_id)
   end
 end
