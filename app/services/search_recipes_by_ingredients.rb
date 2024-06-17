fa65b2cf9b21b2c77c8b723596fb78148364d9e8
Benjamin Jackson <benj@betterrentals.com.au>
2024-06-17 16:09:47 +1000

    ready to start filters again..

--- app/services/search_recipes_by_ingredients.rb
+++ app/services/search_recipes_by_ingredients.rb
@@ -37,13 +37,17 @@ class SearchRecipesByIngredients
   end
 
   def create_recipe(result_hash)
-    method = "No instructions provided!" if result_hash["analyzedInstructions"].nil?
+    if result_hash["analyzedInstructions"][0] != nil
+      method = result_hash["analyzedInstructions"][0]["steps"]
+    else
+      method = [{step: "No method exists!"}]
+    end
     cuisine = result_hash["cuisines"].empty? ? "None" : result_hash["cuisines"].first
     recipe = Recipe.create(title: result_hash["title"],
       summary: result_hash["summary"],
       image_url: result_hash["image"],
       cuisine: cuisine,
-      method: (result_hash["analyzedInstructions"][0]["steps"] || method),
+      method: method,
       servings: result_hash["servings"],
       cook_time: result_hash["readyInMinutes"],
       spoonacular_id: result_hash["id"]
